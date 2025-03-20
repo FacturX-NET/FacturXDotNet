46ff1ecb2051c3388d87a5007c7bad5024167b22
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-03-21 00:44:27 +0100

    implement PDF/A rule

--- FacturXDotNet/Validation/BusinessRules/Hybrid/BrHybrid02.cs
+++ FacturXDotNet/Validation/BusinessRules/Hybrid/BrHybrid02.cs
@@ -1,10 +1,11 @@
-﻿namespace FacturXDotNet.Validation.BusinessRules.Hybrid;
+﻿using FacturXDotNet.Models.XMP;
+
+namespace FacturXDotNet.Validation.BusinessRules.Hybrid;
 
 record BrHybrid02() : HybridBusinessRule(
     "BR-HYBRID-02",
     "The PDF envelope of a hybrid document SHALL use the PDF/A-3 standard.Optionally, a PDF/A-4f file (ISO 19005-4, based on PDF 2.0 ISO 32000-2:2020) is allowed."
 )
 {
-    // TODO
-    public override bool Check(FacturX invoice) => true;
+    public override bool Check(FacturX invoice) => invoice.XmpMetadata.PdfAIdentification is { Conformance: XmpPdfAConformanceLevel.A, Part: >= 3 };
 }
