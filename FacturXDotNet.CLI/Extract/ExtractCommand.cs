20a285a6494594bf0c1ed6b3145935777b0886f5
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-03-22 11:36:13 +0100

    try commandlineparser

---
+++ FacturXDotNet.CLI/Extract/ExtractCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace FacturXDotNet.CLI.Extract;
+
+static class ExtractCommand
+{
+    public static async Task RunAsync(ExtractOption options)
+    {
+        Console.WriteLine($"Extracting {options.Path}...");
+        Console.WriteLine($"CII {options.CiiPath ?? "(null)"}...");
+        Console.WriteLine($"XMP {options.XmpPath ?? "(null)"}...");
+    }
+}
--- FacturXDotNet.CLI/Program.cs
+++ FacturXDotNet.CLI/Program.cs
@@ -1,14 +1,8 @@
-﻿using System.Text.Json;
-using System.Text.Json.Serialization;
-using FacturXDotNet;
-using FacturXDotNet.CLI;
-using FacturXDotNet.Models;
-using FacturXDotNet.Parsing;
-using FacturXDotNet.Parsing.CII;
-using FacturXDotNet.Parsing.XMP;
-using FacturXDotNet.Validation;
-using FacturXDotNet.Validation.BusinessRules;
-using Microsoft.Extensions.Configuration;
+﻿using System.Diagnostics;
+using System.Reflection;
+using CommandLine;
+using CommandLine.Text;
+using FacturXDotNet.CLI.Extract;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Extensions.Logging;
@@ -21,99 +15,65 @@ ILogger logger = loggerFactory.CreateLogger("Program");
 
 try
 {
-    SourceGenerationContext sourceGenerationContext =
-        new(new JsonSerializerOptions(JsonSerializerDefaults.Web) { WriteIndented = true, Converters = { new JsonStringEnumConverter() } });
-
-    IConfigurationRoot configuration = new ConfigurationBuilder().AddEnvironmentVariables().Build();
-    Options options = configuration.Get<Options>() ?? new Options();
-    string environment = options.Environment ?? string.Empty;
-
-    await using FileStream example = File.OpenRead(@"D:\source\repos\FacturXDotNet\FacturXDotNet.CLI\Examples\Facture_F20220023-LE_FOURNISSEUR-POUR-LE_CLIENT_MINIMUM.pdf");
-
-    FacturXParser parser = new(
-        new FacturXParserOptions
+    Parser parser = new(
+        settings =>
         {
-            Xmp =
-            {
-                Logger = environment.Equals("development", StringComparison.InvariantCultureIgnoreCase) ? loggerFactory.CreateLogger<XmpMetadataParser>() : null
-            },
-            Cii =
-            {
-                Logger = environment.Equals("development", StringComparison.InvariantCultureIgnoreCase) ? loggerFactory.CreateLogger<CrossIndustryInvoiceParser>() : null
-            }
+            settings.AutoVersion = true;
+            settings.AutoHelp = true;
+            settings.CaseSensitive = false;
+            settings.CaseInsensitiveEnumValues = true;
+            settings.HelpWriter = null;
         }
     );
 
-    FacturX facturX = await parser.ParseFacturXPdfAsync(example);
-    logger.LogInformation("-------------");
-    logger.LogInformation("     XMP");
-    logger.LogInformation("-------------");
-    logger.LogInformation("{XMP}", JsonSerializer.Serialize(facturX.XmpMetadata, sourceGenerationContext.XmpMetadata));
-
-    logger.LogInformation("-------------");
-    logger.LogInformation("     CII");
-    logger.LogInformation("-------------");
-    logger.LogInformation("{CII}", JsonSerializer.Serialize(facturX.CrossIndustryInvoice, sourceGenerationContext.CrossIndustryInvoice));
-
-    FacturXValidator validator = new();
-    FacturXValidationResult validationResult = validator.GetValidationResult(facturX);
-
-    logger.LogInformation("-------------");
-    logger.LogInformation(" VALIDATION");
-    logger.LogInformation("-------------");
-
-    logger.LogInformation("Success: {Success}", validationResult.Success);
-    logger.LogInformation("Actual profile: {Profile}", validationResult.ValidProfiles.GetMaxProfile());
-
-    logger.LogInformation("Details:");
-
-    if (validationResult.Fatal.Count > 0)
-    {
-        logger.LogError("- Failed: ({FailedCount})", validationResult.Fatal.Count);
-    }
-    else
-    {
-        logger.LogInformation("- Failed: ({FailedCount})", validationResult.Fatal.Count);
-    }
-    foreach (FacturXBusinessRule rule in validationResult.Fatal)
-    {
-        logger.LogError("  - KO {Rule}", rule.Format());
-    }
-
-    logger.LogInformation("- Passed: ({PassedCount})", validationResult.Passed.Count);
-    foreach (FacturXBusinessRule? rule in validationResult.Passed)
-    {
-        logger.LogInformation("  - OK {Rule}", rule.Format());
-    }
-
-    logger.LogInformation("- Expected to fail: ({ExpectedToFailCount})", validationResult.ExpectedToFail.Count);
-    foreach (FacturXBusinessRule? rule in validationResult.ExpectedToFail)
-    {
-        logger.LogInformation("  - KO {Rule}", rule.Format());
-    }
-
-    logger.LogInformation("- Skipped: ({SkippedCount})", validationResult.Skipped.Count);
-    foreach (FacturXBusinessRule? rule in validationResult.Skipped)
-    {
-        logger.LogInformation("  - ?? {Rule}", rule.Format());
-    }
+    ParserResult<ExtractOption>? parsed = parser.ParseArguments<ExtractOption>(args);
+    await parsed.WithParsedAsync(ExtractCommand.RunAsync);
+    await parsed.WithNotParsedAsync(errs => NotParsedAsync(parsed, errs));
 }
 catch (Exception exn)
 {
     logger.LogCritical(exn, "Unhandled exception.");
 }
+return;
 
-namespace FacturXDotNet.CLI
+async Task NotParsedAsync(ParserResult<ExtractOption> parsed1, IEnumerable<Error> errs)
 {
-    class Options
+    IEnumerable<Error> enumerable = errs as Error[] ?? errs.ToArray();
+    if (enumerable.IsVersion())
     {
-        public string? Environment { get; set; }
+        await Console.Out.WriteLineAsync(GetVersion());
     }
+    else if (enumerable.IsHelp())
+    {
+        await Console.Out.WriteLineAsync(GetHelp(parsed1));
+    }
+    else
+    {
+        await Console.Error.WriteLineAsync(GetHelp(parsed1));
+    }
+}
 
-    [JsonSourceGenerationOptions(WriteIndented = true)]
-    [JsonSerializable(typeof(CrossIndustryInvoice))]
-    [JsonSerializable(typeof(XmpMetadata))]
-    partial class SourceGenerationContext : JsonSerializerContext
-    {
-    }
+string GetVersion()
+{
+    Assembly assembly = Assembly.GetExecutingAssembly();
+    FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+    return fileVersionInfo.ProductVersion ?? "~dev";
+}
+
+HelpText GetHelp(ParserResult<ExtractOption> parserResult)
+{
+    HelpText? helpText = HelpText.AutoBuild(
+        parserResult,
+        h =>
+        {
+            h.AdditionalNewLineAfterOption = false;
+            h.AddEnumValuesToHelpText = true;
+            h.Heading = $"FacturX.NET {GetVersion()}";
+            h.Copyright = "Copyright (C) 2025 Ismail Bennani";
+            return HelpText.DefaultParsingErrorsHandler(parserResult, h);
+        },
+        e => e
+    );
+
+    return helpText;
 }
