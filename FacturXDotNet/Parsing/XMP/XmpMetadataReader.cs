54c1382100b466f590ded558360a4005c4acf6c6
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-03-25 00:16:10 +0100

    move xmp parsing hack in FacturXDocument to XmpMetadataReader

--- FacturXDotNet/Parsing/XMP/XmpMetadataReader.cs
+++ FacturXDotNet/Parsing/XMP/XmpMetadataReader.cs
@@ -1,11 +1,12 @@
-﻿using TurboXml;
+﻿using System.Text.RegularExpressions;
+using TurboXml;
 
 namespace FacturXDotNet.Parsing.XMP;
 
 /// <summary>
 ///     Read a <see cref="XmpMetadata" /> from an XML stream.
 /// </summary>
-public class XmpMetadataReader(XmpMetadataReaderOptions? options = null)
+public partial class XmpMetadataReader(XmpMetadataReaderOptions? options = null)
 {
     readonly XmpMetadataReaderOptions _options = options ?? new XmpMetadataReaderOptions();
 
@@ -14,11 +15,47 @@ public class XmpMetadataReader(XmpMetadataReaderOptions? options = null)
     /// </summary>
     public XmpMetadata Read(Stream stream)
     {
-        XmpMetadata result = new();
-        XmpMetadataXmlReadHandler handler = new(result, _options.Logger);
+        long position = stream.Position;
+        Span<byte> firstChars = stackalloc byte[5];
+        stream.ReadExactly(firstChars);
+        stream.Seek(position, SeekOrigin.Begin);
 
-        XmlParser.Parse(stream, ref handler);
+        MemoryStream? transformedStream = null;
+        try
+        {
+            if (firstChars[0] == '<' && (firstChars[1] != '?' || firstChars[2] != 'x' || firstChars[3] != 'm' || firstChars[4] != 'l'))
+            {
+                // TODO: avoid these two extra copies, it is only required because TurboXML doesn't support the <?xpacket...?> processing instructions
+                // an issue has been opened to address this: https://github.com/xoofx/TurboXml/issues/6
+                // I need to fix this in the library, but it will take some time
 
-        return result;
+                using StreamReader reader = new(stream, leaveOpen: true);
+                string content = reader.ReadToEnd();
+                string transformedContent = PacketInstructions().Replace(content, string.Empty);
+
+                transformedStream = new MemoryStream(transformedContent.Length + 54);
+                using StreamWriter writer = new(transformedStream, leaveOpen: true);
+                writer.Write($"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>{Environment.NewLine}""");
+                writer.Write(transformedContent);
+                writer.Flush();
+                transformedStream.Seek(0, SeekOrigin.Begin);
+
+                stream = transformedStream;
+            }
+
+            XmpMetadata result = new();
+            XmpMetadataXmlReadHandler handler = new(result, _options.Logger);
+
+            XmlParser.Parse(stream, ref handler);
+
+            return result;
+        }
+        finally
+        {
+            transformedStream?.Dispose();
+        }
     }
+
+    [GeneratedRegex("\\s*<\\?xpacket.*?\\?>\\s*", RegexOptions.ExplicitCapture | RegexOptions.Compiled)]
+    private static partial Regex PacketInstructions();
 }
