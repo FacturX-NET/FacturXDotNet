3a524647b2fcac7b5ca50b2a37c64adead08bee9
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-04-06 00:47:08 +0200

    change layout of pdf/a identification

--- src/FacturXDotNet.WebEditor/src/app/features/editor/tabs/xmp/xmp-form/xmp-form-pdf-a-identification.component.ts
+++ src/FacturXDotNet.WebEditor/src/app/features/editor/tabs/xmp/xmp-form/xmp-form-pdf-a-identification.component.ts
@@ -22,25 +22,27 @@ import { ControlContainer, FormsModule, ReactiveFormsModule } from '@angular/for
         </p>
 
         <div class="mb-3">
-          <label class="form-label" for="pdfAIdentificationAmendment">Amendment</label>
-          <input id="pdfAIdentificationAmendment" class="editor__control form-control" formControlName="amendment" aria-describedby="pdfAIdentificationAmendmentHelp" />
-          <p id="pdfAIdentificationAmendmentHelp" class="form-text">Optional PDF/A amendment identifier.</p>
-        </div>
-
-        <div class="mb-3">
-          <label class="form-label" for="pdfAIdentificationConformance">Conformance</label>
-          <select id="pdfAIdentificationConformance" class="editor__control form-select" formControlName="conformance" aria-describedby="pdfAIdentificationConformanceHelp">
-            <option value="" class="text-body-tertiary" selected>Choose a conformance level</option>
-            <option value="A">A</option>
-            <option value="B">B</option>
-          </select>
-          <p id="pdfAIdentificationConformanceHelp" class="form-text">PDF/A conformance level: A or B.</p>
-        </div>
-
-        <div class="mb-3">
-          <label class="form-label" for="pdfAIdentificationPart">Part</label>
-          <input id="pdfAIdentificationPart" class="editor__control form-control" formControlName="part" aria-describedby="pdfAIdentificationPartHelp" />
-          <p id="pdfAIdentificationPartHelp" class="form-text">PDF/A version identifier.</p>
+          <div class="row">
+            <div class="col-3">
+              <label class="form-label" for="pdfAIdentificationPart">Part</label>
+              <input id="pdfAIdentificationPart" class="editor__control form-control" formControlName="part" aria-describedby="pdfAIdentificationPartHelp" />
+              <p id="pdfAIdentificationPartHelp" class="form-text">PDF/A version identifier.</p>
+            </div>
+            <div class="col-4">
+              <label class="form-label" for="pdfAIdentificationConformance">Conformance</label>
+              <select id="pdfAIdentificationConformance" class="editor__control form-select" formControlName="conformance" aria-describedby="pdfAIdentificationConformanceHelp">
+                <option value="" class="text-body-tertiary" selected>Choose a conformance level</option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+              </select>
+              <p id="pdfAIdentificationConformanceHelp" class="form-text">PDF/A conformance level: A or B.</p>
+            </div>
+            <div class="col">
+              <label class="form-label" for="pdfAIdentificationAmendment">Amendment</label>
+              <input id="pdfAIdentificationAmendment" class="editor__control form-control" formControlName="amendment" aria-describedby="pdfAIdentificationAmendmentHelp" />
+              <p id="pdfAIdentificationAmendmentHelp" class="form-text">Optional PDF/A amendment identifier.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
