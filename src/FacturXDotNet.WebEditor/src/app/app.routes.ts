57aa15ab4184e3c048e5f37cde0160b7e03f1fa7
Ismail Bennani <ismail.lahkim.bennani@gmail.com>
2025-04-08 14:21:25 +0200

    chore: change route of editor page (#50)

--- src/FacturXDotNet.WebEditor/src/app/app.routes.ts
+++ src/FacturXDotNet.WebEditor/src/app/app.routes.ts
@@ -1,18 +1,11 @@
 import { Routes } from '@angular/router';
+import { routes as editorRoutes } from './features/editor/editor.routes';
 
 export const routes: Routes = [
-  {
-    path: 'editor',
-    loadChildren: () => import('./features/editor/editor.routes').then((m) => m.routes),
-  },
+  ...editorRoutes,
   {
     path: 'about',
     loadChildren: () => import('./features/about/about.routes').then((m) => m.routes),
-  },
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'editor',
   },
   {
     path: '**',
