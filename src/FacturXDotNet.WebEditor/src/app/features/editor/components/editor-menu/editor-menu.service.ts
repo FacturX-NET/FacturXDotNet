3be136b3615bb6d41c701685277ac7e3e0758c79
Ismail Bennani <ismail.lahkim.bennani@gmail.com>
2025-04-08 18:48:55 +0200

    fix: use matching version of pdfjs (#54)

--- src/FacturXDotNet.WebEditor/src/app/features/editor/components/editor-menu/editor-menu.service.ts
+++ src/FacturXDotNet.WebEditor/src/app/features/editor/components/editor-menu/editor-menu.service.ts
@@ -22,7 +22,7 @@ export class EditorMenuService {
   private destroyRef = inject(DestroyRef);
 
   constructor() {
-    pdf.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.0.375/pdf.worker.min.mjs';
+    pdf.GlobalWorkerOptions.workerSrc = 'https://app.unpkg.com/pdfjs-dist@5.1.91/files/build/pdf.worker.min.mjs';
   }
 
   createNewDocument(): Observable<void> {
