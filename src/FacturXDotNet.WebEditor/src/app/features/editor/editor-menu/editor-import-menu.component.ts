6255cd36861aa4b0a6db1688ddf2a8f19f7f6ee6
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-04-13 12:19:56 +0200

    Refactor dropdowns to use ng-bootstrap components
    Replaced Bootstrap's native dropdowns with ng-bootstrap components for consistency and improved functionality. Updated all relevant menu components accordingly.

--- src/FacturXDotNet.WebEditor/src/app/features/editor/editor-menu/editor-import-menu.component.ts
+++ src/FacturXDotNet.WebEditor/src/app/features/editor/editor-menu/editor-import-menu.component.ts
@@ -3,20 +3,20 @@ import { ToastService } from '../../../core/toasts/toast.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { EditorMenuService } from './editor-menu.service';
 import { toastError } from '../../../core/utils/toast-error';
+import { NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-editor-import-menu',
   template: `
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle px-4 text-light" role="button" data-bs-toggle="dropdown" aria-expanded="false">Import</a>
-      <ul class="dropdown-menu">
-        <li>
-          <a class="dropdown-item" href="javascript:void 0;" (click)="importCrossIndustryInvoice()">Import Cross-Industry Invoice data</a>
-          <a class="dropdown-item" href="javascript:void 0;" (click)="importPdfImage()">Import PDF image</a>
-        </li>
-      </ul>
+    <li class="nav-item" ngbDropdown>
+      <button id="editor-import-menu" class="nav-link px-4 text-light" ngbDropdownToggle>Import</button>
+      <div ngbDropdownMenu aria-labelledby="editor-import-menu">
+        <button (click)="importCrossIndustryInvoice()" ngbDropdownItem>Import Cross-Industry Invoice data</button>
+        <button (click)="importPdfImage()" ngbDropdownItem>Import PDF image</button>
+      </div>
     </li>
   `,
+  imports: [NgbDropdown, NgbDropdownToggle, NgbDropdownMenu, NgbDropdownItem],
 })
 export class EditorImportMenuComponent {
   private editorMenuService = inject(EditorMenuService);
--- src/FacturXDotNet.WebEditor/src/app/features/editor/tabs/cii/cii-menu/cii-menu-details-dropdown.component.ts
+++ src/FacturXDotNet.WebEditor/src/app/features/editor/tabs/cii/cii-menu/cii-menu-details-dropdown.component.ts
@@ -1,45 +1,46 @@
 import { Component, inject, input } from '@angular/core';
 import { EditorSettings, EditorSettingsService } from '../../../editor-settings.service';
+import { NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-cii-menu-details-dropdown',
+  imports: [NgbDropdown, NgbDropdownMenu, NgbDropdownToggle, NgbDropdownItem],
   template: `
     @if (settings(); as settings) {
-      <div class="dropdown" data-bs-toggle="tooltip" data-bs-title="Display settings">
-        <button class="btn btn-link" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-eye"></i></button>
-        <ul class="dropdown-menu dropdown-menu-end">
-          <li>
-            <a class="dropdown-item" href="javascript:void 0;" (click)="toggleBusinessRules()" [class.text-body-tertiary]="settings.showBusinessRules !== true">
-              @if (settings.showBusinessRules === true) {
-                <i class="bi bi-eye"></i>
-              } @else {
-                <i class="bi bi-eye-slash"></i>
-              }
-              Business Rules
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="javascript:void 0;" (click)="toggleRemarks()" [class.text-body-tertiary]="settings.showRemarks !== true">
-              @if (settings.showRemarks === true) {
-                <i class="bi bi-eye"></i>
-              } @else {
-                <i class="bi bi-eye-slash"></i>
-              }
-              Remarks
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="javascript:void 0;" (click)="toggleChorusProRemarks()" [class.text-body-tertiary]="settings.showChorusProRemarks !== true">
-              @if (settings.showChorusProRemarks === true) {
-                <i class="bi bi-eye"></i>
-              } @else {
-                <i class="bi bi-eye-slash"></i>
-              }
-              Chorus Pro Remarks
-            </a>
-          </li>
-        </ul>
+      <div ngbDropdown data-bs-toggle="tooltip" data-bs-title="Display settings">
+        <button id="cii-menu-details" class="btn btn-link hide-toggle" ngbDropdownToggle><i class="bi bi-eye"></i></button>
+        <div ngbDropdownMenu aria-labelledby="cii-menu-details">
+          <button (click)="toggleBusinessRules()" [class.text-body-tertiary]="settings.showBusinessRules !== true" ngbDropdownItem>
+            @if (settings.showBusinessRules === true) {
+              <i class="bi bi-eye"></i>
+            } @else {
+              <i class="bi bi-eye-slash"></i>
+            }
+            Business Rules
+          </button>
+          <button (click)="toggleRemarks()" [class.text-body-tertiary]="settings.showRemarks !== true" ngbDropdownItem>
+            @if (settings.showRemarks === true) {
+              <i class="bi bi-eye"></i>
+            } @else {
+              <i class="bi bi-eye-slash"></i>
+            }
+            Remarks
+          </button>
+          <button (click)="toggleChorusProRemarks()" [class.text-body-tertiary]="settings.showChorusProRemarks !== true" ngbDropdownItem>
+            @if (settings.showChorusProRemarks === true) {
+              <i class="bi bi-eye"></i>
+            } @else {
+              <i class="bi bi-eye-slash"></i>
+            }
+            Chorus Pro Remarks
+          </button>
+        </div>
       </div>
+    }
+  `,
+  styles: `
+    .hide-toggle::after {
+      content: none;
     }
   `,
 })
