37ab31b854c1b5eae8e06f0b68ad00af7f74b9ea
Ismail Bennani <ismail.lahkim.bennani@gmail.com>
2025-04-16 00:22:05 +0200

    feat: use Roboto and embed it in the app (#70)

---
+++ src/FacturXDotNet/Generation/PDF/Generators/Standard/RobotoFontResolver.cs
@@ -0,0 +1,40 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Fonts;
+
+namespace FacturXDotNet.Generation.PDF.Generators.Standard;
+
+class RobotoFontResolver : IFontResolver
+{
+    public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
+    {
+        if (familyName != "Roboto")
+        {
+            return null;
+        }
+
+        string fontName = italic
+            ? bold ? "Roboto-BoldItalic" : "Roboto-Regular"
+            : bold
+                ? "Roboto-Bold"
+                : "Roboto-Regular";
+
+        return new FontResolverInfo(fontName, XStyleSimulations.None);
+    }
+
+    byte[]? IFontResolver.GetFont(string faceName) => ReadFont(faceName);
+
+    static byte[]? ReadFont(string faceName)
+    {
+        Stream? fontStream = typeof(RobotoFontResolver).Assembly.GetManifestResourceStream($"FacturXDotNet.Resources.Fonts.Roboto.{faceName}.ttf");
+        if (fontStream == null)
+        {
+            return null;
+        }
+
+        using Stream _ = fontStream;
+        byte[] result = new byte[fontStream.Length];
+        fontStream.ReadExactly(result);
+
+        return result;
+    }
+}
