1cffb1cddfd055659a275637fde277eaf69cd6b4
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-04-18 12:09:11 +0200

    Update navigation handling in editor left pane header
    Replaced hardcoded router URLs with dynamic tab computation based on the current navigation state. This improves maintainability and supports URL-based tab selection using `toSignal` and `computed` constructs. Unused imports were also removed for cleanup.

--- src/FacturXDotNet.WebEditor/src/app/features/editor/editor-header/editor-left-pane-header.component.ts
+++ src/FacturXDotNet.WebEditor/src/app/features/editor/editor-header/editor-left-pane-header.component.ts
@@ -1,17 +1,19 @@
-import { Component, computed, effect, ElementRef, inject, input, signal, Signal, viewChild } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { EditorSavedState } from '../editor-state.service';
 import { EditorSettings } from '../editor-settings.service';
-import { Router, RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { NgbNav, NgbNavItem, NgbNavLink, NgbNavLinkBase } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { EditorResponsivenessService } from '../editor-responsiveness.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { distinct, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-editor-left-pane-header',
   imports: [NgbNav, NgbNavItem, NgbNavLink, RouterLink, FormsModule, NgbNavLinkBase],
   template: `
-    <ul ngbNav [activeId]="router.url" class="nav-tabs px-4">
-      <li ngbNavItem="/xmp">
+    <ul ngbNav [activeId]="tab()" class="nav-tabs px-4">
+      <li ngbNavItem="xmp">
         <a ngbNavLink role="button" routerLink="/xmp">
           <i class="bi bi-info-lg pe-1"></i>
           @if (!folded()) {
@@ -19,7 +21,7 @@ import { EditorResponsivenessService } from '../editor-responsiveness.service';
           }
         </a>
       </li>
-      <li ngbNavItem="/cii">
+      <li ngbNavItem="cii">
         <a ngbNavLink role="button" routerLink="/cii">
           <i class="bi bi-code pe-1"></i>
           @if (!folded()) {
@@ -27,7 +29,7 @@ import { EditorResponsivenessService } from '../editor-responsiveness.service';
           }
         </a>
       </li>
-      <li ngbNavItem="/attachments">
+      <li ngbNavItem="attachments">
         <a ngbNavLink role="button" routerLink="/attachments">
           <i class="bi bi-paperclip pe-1"></i>
           @if (!folded()) {
@@ -37,7 +39,7 @@ import { EditorResponsivenessService } from '../editor-responsiveness.service';
         </a>
       </li>
       <div class="flex-grow-1"><!--spacer--></div>
-      <li ngbNavItem="/settings">
+      <li ngbNavItem="settings">
         <a ngbNavLink role="button" routerLink="/settings">
           <i class="bi bi-gear pe-1"></i>
           @if (!folded()) {
@@ -53,8 +55,22 @@ export class EditorLeftPaneHeaderComponent {
   settings = input.required<EditorSettings>();
 
   protected router = inject(Router);
-  protected editorResponsivenessService = inject(EditorResponsivenessService);
+  protected url = toSignal(
+    this.router.events.pipe(
+      filter((evt) => evt instanceof NavigationEnd),
+      map((evt) => evt.urlAfterRedirects),
+      filter((url) => url !== undefined),
+      distinct(),
+    ),
+    { initialValue: this.router.url },
+  );
+  protected tab = computed(() => {
+    const url = this.url();
+    const urlSplit = url.split('/');
+    return urlSplit.find((f) => f.length > 0);
+  });
 
+  protected editorResponsivenessService = inject(EditorResponsivenessService);
   protected small = this.editorResponsivenessService.smallLeftColumn;
   protected folded = this.editorResponsivenessService.foldLeftColumn;
 }
