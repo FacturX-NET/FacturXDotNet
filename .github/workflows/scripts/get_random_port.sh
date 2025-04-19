b22b3226fbb4596f84e0fad057b95ce7d8fe9e62
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-04-19 23:11:23 +0200

    use bash

--- .github/workflows/scripts/get_random_port.sh
+++ .github/workflows/scripts/get_random_port.sh
@@ -1,4 +1,6 @@
-﻿# Find random available port
+﻿#!/usr/bin/env bash
+
+# Find random available port
 # Copied from https://unix.stackexchange.com/a/423052
 
 comm -23 <(seq 49152 65535 | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 1
