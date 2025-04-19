953c0abf4896a97b65c23f16929f0a7670f84b92
ismailbennani <ismail.lahkim.bennani@gmail.com>
2025-04-19 23:06:22 +0200

    again?

--- .github/workflows/scripts/get_random_port.sh
+++ .github/workflows/scripts/get_random_port.sh
@@ -1,1 +1,4 @@
-﻿comm -23 <(seq 49152 65535 | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 1
+﻿# Find random available port
+# Copied from https://unix.stackexchange.com/a/423052
+
+comm -23 <(seq 49152 65535 | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 1
